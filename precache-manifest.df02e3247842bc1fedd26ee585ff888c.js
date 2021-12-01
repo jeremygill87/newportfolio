0b7937be989e7a0add6cd4650a848f97c0e9e9fa
Jeremy Gill <54640283+jeremygill87@users.noreply.github.com>
2021-12-01 13:12:27 -0500

    Updates

---
+++ precache-manifest.df02e3247842bc1fedd26ee585ff888c.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bcc57ba1620f0659a3d9bb57b037e0c",
+    "url": "/newportfolio/index.html"
+  },
+  {
+    "revision": "26f3f22dc54972b4d802",
+    "url": "/newportfolio/static/css/2.8850a738.chunk.css"
+  },
+  {
+    "revision": "a4458a58df0d4c6b368d",
+    "url": "/newportfolio/static/css/main.76689215.chunk.css"
+  },
+  {
+    "revision": "26f3f22dc54972b4d802",
+    "url": "/newportfolio/static/js/2.e02a24ee.chunk.js"
+  },
+  {
+    "revision": "332bf1b0d236b7fc5455596e6356764c",
+    "url": "/newportfolio/static/js/2.e02a24ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4458a58df0d4c6b368d",
+    "url": "/newportfolio/static/js/main.ad621fe5.chunk.js"
+  },
+  {
+    "revision": "8a33ca4eb75dc7764148",
+    "url": "/newportfolio/static/js/runtime-main.7e55ce76.js"
+  },
+  {
+    "revision": "f12278982777ff3c3bbb5c22c788c76f",
+    "url": "/newportfolio/static/media/baggage.f1227898.jpg"
+  },
+  {
+    "revision": "ed3c55664ab6acef0d0f2f193f305bcc",
+    "url": "/newportfolio/static/media/crystals.ed3c5566.jpg"
+  },
+  {
+    "revision": "67af05e12311bac06327b43ca31e166b",
+    "url": "/newportfolio/static/media/earth.67af05e1.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/newportfolio/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/newportfolio/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/newportfolio/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/newportfolio/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/newportfolio/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "768998c65ce07675ede8536017d4f6ce",
+    "url": "/newportfolio/static/media/image1.768998c6.jpg"
+  },
+  {
+    "revision": "8de6195d8faafe31ac0a842eb0cbe1a9",
+    "url": "/newportfolio/static/media/image2.8de6195d.jpg"
+  },
+  {
+    "revision": "28470933ba170d7cd8d5495969722a5d",
+    "url": "/newportfolio/static/media/image3.28470933.jpg"
+  },
+  {
+    "revision": "0b37f43494ce5a35f3e1575612048750",
+    "url": "/newportfolio/static/media/image4.0b37f434.jpg"
+  },
+  {
+    "revision": "8d16c4aff0e7bd3db1b8bab9619aea9c",
+    "url": "/newportfolio/static/media/image5.8d16c4af.jpg"
+  },
+  {
+    "revision": "62b7cc7591147260c113823ded8f6901",
+    "url": "/newportfolio/static/media/image6.62b7cc75.jpg"
+  },
+  {
+    "revision": "05acfdb568b3df49ad31355b19495d4a",
+    "url": "/newportfolio/static/media/ionicons.05acfdb5.woff"
+  },
+  {
+    "revision": "24712f6c47821394fba7942fbb52c3b2",
+    "url": "/newportfolio/static/media/ionicons.24712f6c.ttf"
+  },
+  {
+    "revision": "2c2ae068be3b089e0a5b59abb1831550",
+    "url": "/newportfolio/static/media/ionicons.2c2ae068.eot"
+  },
+  {
+    "revision": "621bd386841f74e0053cb8e67f8a0604",
+    "url": "/newportfolio/static/media/ionicons.621bd386.svg"
+  },
+  {
+    "revision": "d77e20cc4ac6c274e01707f619d96797",
+    "url": "/newportfolio/static/media/marvel.d77e20cc.jpg"
+  },
+  {
+    "revision": "edac7f9a7e3fa65d9afc7023179e8f6d",
+    "url": "/newportfolio/static/media/me.edac7f9a.jpg"
+  },
+  {
+    "revision": "91d3bcdbc627348e662d6e14c55ef8a3",
+    "url": "/newportfolio/static/media/myAvatar.91d3bcdb.png"
+  }
+]);
